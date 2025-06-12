4fa0d2d1695cad4817c935c7175a526adce5d023
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-06-12 15:20:46 +0700

    init

--- src/linux/zrun.sh
+++ src/linux/zrun.sh
@@ -1,3 +1,3 @@
 mv trigger_serial.txt trigger_serial.txt_
 export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH:/usr/local/lib
-./test_main --is_baudrate=9600 --is_cfg=modbus.cfg --is_port=/dev/ttyUSB0
+./test_ModbusAsync --is_baudrate=9600 --is_cfg=modbus.cfg --is_port=/dev/ttyUSB0
